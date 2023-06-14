e10b06c24382fd47b4a88bcb36dce482a16c5820
marwan-elabhar <marwan.a.elabhar@gmail.com>
2023-06-14 11:13:26 +0300

    add todolist component

---
+++ src/components/TodoList.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import Item from "./Item.vue";
+import type { TodoItem } from "types";
+import type { PropType } from "vue";
+
+const props = defineProps({
+  todoList: {
+    type: Object as PropType<TodoItem[]>,
+    required: true,
+  },
+});
+
+const emit = defineEmits<{
+  (e: "delete:item", id: string): void;
+}>();
+
+function deleteItem(id: string): void {
+    emit('delete:item', id)
+}
+</script>
+
+<template>
+  <div>
+    <div v-for="todo in todoList" :key="todo.id">
+      <Item :item="todo" @delete:item="deleteItem" />
+    </div>
+  </div>
+</template>
+
+
+
+<style lang="scss" scoped>
+</style>

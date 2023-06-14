1e032f23b1e15366fd4f9c75275367cb07589d76
marwan-elabhar <marwan.a.elabhar@gmail.com>
2023-06-14 11:13:35 +0300

    add item component

---
+++ src/components/Item.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import type { TodoItem } from "types";
+import type { PropType } from "vue";
+
+const props = defineProps({
+  item: {
+    type: Object as PropType<TodoItem>,
+    required: true,
+  },
+});
+
+const emit = defineEmits<{
+  (e: 'delete:item', id: string): void
+}>()
+function deleteItem() :void {
+    emit('delete:item', props.item.id)
+}
+</script>
+
+<template>
+  <div>
+    <p>{{ item.text }}</p>
+    <button>edit</button>
+    <button @click="deleteItem">delete</button>
+  </div>
+</template>
+
+
+
+<style lang="scss" scoped>
+</style>
